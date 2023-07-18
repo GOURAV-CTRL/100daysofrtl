859f74637b2ad30ccc718dbb2bcc78c7dc5b414c
GOURAV-CTRL <120794692+GOURAV-CTRL@users.noreply.github.com>
2023-07-18 18:53:32 +0530

    Add files via upload

---
+++ alu_decoder_RISC_DAY3.v
@@ -0,0 +1,19 @@
+
+
+module alu_decoder(ALUOp,funct3,funct7,op,ALUControl);
+
+    input [1:0]ALUOp;
+    input [2:0]funct3;
+    input [6:0]funct7,op;
+    output [2:0]ALUControl;
+
+    
+    assign ALUControl = (ALUOp == 2'b00) ? 3'b000 :
+                        (ALUOp == 2'b01) ? 3'b001 :
+                        ((ALUOp == 2'b10) & (funct3 == 3'b000) & ({op[5],funct7[5]} == 2'b11)) ? 3'b001 : 
+                        ((ALUOp == 2'b10) & (funct3 == 3'b000) & ({op[5],funct7[5]} != 2'b11)) ? 3'b000 : 
+                        ((ALUOp == 2'b10) & (funct3 == 3'b010)) ? 3'b101 : 
+                        ((ALUOp == 2'b10) & (funct3 == 3'b110)) ? 3'b011 : 
+                        ((ALUOp == 2'b10) & (funct3 == 3'b111)) ? 3'b010 : 
+                                                                  3'b000 ;
+endmodule
